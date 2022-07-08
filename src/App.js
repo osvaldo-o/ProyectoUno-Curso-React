75a0186f3b013906e27841f806bf4c63a1ed77f8
osvaldo <osvaldo.guerra.c008@gmail.com>
2022-07-08 15:21:58 -0500

    Proyecto terminado con retos cumplidos

--- src/App.js
+++ src/App.js
@@ -12,21 +12,27 @@ function App() {
           image='1'
           position='Software Engineer'
           business='Amazon'
-          testimony='Its scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life.'/>
+          testimonyInicio='Its scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer.'
+          testimonyBold=' freeCodeCamp changed my life.'
+          testimonyEnd='' />
         <Testimony 
           name='Sarah Chima'
           country='Nigeria'
           image='2'
           position='Software Engineer'
           business='ChatDesk'
-          testimony='freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.'/>
+          testimonyInicio=''
+          testimonyBold='freeCodeCamp was the gateway to my career'
+          testimonyEnd='as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.' />
           <Testimony 
           name='Emma Bostian'
           country='Sweden'
           image='3'
           position='Software Engineer'
           business='Spotify'
-          testimony='Ive always struggled with learning JavaScript. Ive taken many courses but freeCodeCamps course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify.'/>
+          testimonyInicio='Ive always struggled with learning JavaScript. Ive taken many courses but freeCodeCamps course was the one which stuck. Studying JavaScript as well as data structures and algorithms on'
+          testimonyBold=' freeCodeCamp gave me the skills'
+          testimonyEnd=' and confidence I needed to land my dream job as a software engineer at Spotify.' />
       </div>
     </div>
   );
--- src/components/Testimony.jsx
+++ src/components/Testimony.jsx
@@ -4,11 +4,12 @@ import '../stylesheets/Testimony.css';
 function Testimony(props) {
   return (
     <div className='container-testimony'>
-      <img className='image-testimony' src={require(`../image/img-${props.image}.png`)} alt="Image from Emma" />
+      <img className='image-testimony' src={require(`../image/img-${props.image}.png`)} alt={`from an ${props.name}`} />
       <div className='container-text-testimony'>
         <p className='name-testimony'><strong>{props.name}</strong> in {props.country}</p>
         <p className='position-testimony'>{props.position} at <strong>{props.business}</strong></p>
-        <p className='text-testimony'>"{props.testimony}"</p>
+        <p className='text-testimony'>"{props.testimonyInicio} 
+         <strong>{props.testimonyBold}</strong> {props.testimonyEnd}"</p>
       </div>
     </div>
   );
